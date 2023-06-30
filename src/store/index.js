44fb683b73cb770dc6075bb1bac51448d2b6be52
d5ng <dong5ffice@gmail.com>
2023-06-30 19:20:17 +0900

    Providing Store

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import { createStore } from "react-redux"
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        counter: state.counter + 1,
+      }
+
+    case "DECREMENT":
+      return {
+        counter: state.counter - 1,
+      }
+
+    default:
+      return state
+  }
+}
+
+const store = createStore(counterReducer)
+
+export default store
